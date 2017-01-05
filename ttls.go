9ca26d9c0c8f5d48d96eb6afb82846ae70bcb493
Yaron Sumel <yaronsu@gmail.com>
2017-01-05 17:12:12 +0800

    fixes

--- ttls.go
+++ ttls.go
@@ -9,6 +9,7 @@ import (
 	"time"
 	"crypto/rand"
 	"net"
+	"strconv"
 )
 
 type TTLS struct {
@@ -17,17 +18,21 @@ type TTLS struct {
 }
 
 type x509Opts struct {
-	SubjectKeyId *[]byte
-	SerialNumber *big.Int
-	Country      *string
-	Organization *string
+	SubjectKeyId string
+	SerialNumber string
+	Country      string
+	Organization string
 }
 
 func NewTTLSListener(laddr string, x509Opts *x509Opts) (*TTLS, error) {
 	t := TTLS{
 		x509Opts:x509Opts,
 	}
-	listener, err := tls.Listen("tcp", laddr, *t.getConfig())
+	config, err := t.getConfig()
+	if err != nil {
+		return &t, err
+	}
+	listener, err := tls.Listen("tcp", laddr, config)
 	if err != nil {
 		return &t, err
 	}
@@ -35,14 +40,18 @@ func NewTTLSListener(laddr string, x509Opts *x509Opts) (*TTLS, error) {
 	return &t, nil
 }
 
-func (t *TTLS)getConfig() *tls.Config {
+func (t *TTLS)getConfig() (*tls.Config, error) {
+	var err error
 	config := &tls.Config{
 		PreferServerCipherSuites: true,
 		MinVersion: tls.VersionTLS12,
 		Rand:rand.Reader,
 	}
-	config.Certificates = t.getCertificates()
-	return config
+	config.Certificates, err = t.getCertificates()
+	if err != nil {
+		return &tls.Config{}, err
+	}
+	return config, nil
 }
 
 func (t *TTLS)getCertificates() ([]tls.Certificate, error) {
@@ -51,8 +60,10 @@ func (t *TTLS)getCertificates() ([]tls.Certificate, error) {
 		return []tls.Certificate{}, err
 	}
 	return []tls.Certificate{
-		Certificate:[][]byte{x509Cert},
-		PrivateKey:privateKey,
+		{
+			Certificate:[][]byte{x509Cert},
+			PrivateKey:privateKey,
+		},
 	}, nil
 }
 
@@ -60,12 +71,12 @@ func (t *TTLS)getPrivateKey() (*rsa.PrivateKey, error) {
 	return rsa.GenerateKey(rand.Reader, 2048)
 }
 
-func (t *TTLS)getX509Template() *x509.Certificate {
+func (t *TTLS)getX509Template() (*x509.Certificate, error) {
 
 	crt := &x509.Certificate{
 		IsCA : true,
 		BasicConstraintsValid : true,
-		SubjectKeyId : []byte{1, 2, 3},
+		SubjectKeyId : []byte("TTLS"),
 		SerialNumber : big.NewInt(1234),
 		Subject : pkix.Name{
 			Country : []string{"US"},
@@ -78,32 +89,39 @@ func (t *TTLS)getX509Template() *x509.Certificate {
 		KeyUsage : x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
 	}
 
-	if t.x509Opts.SubjectKeyId != nil {
-		crt.SubjectKeyId = *t.x509Opts.SubjectKeyId
+	if t.x509Opts.SubjectKeyId != "" {
+		crt.SubjectKeyId = []byte(t.x509Opts.SubjectKeyId)
 	}
 
-	if t.x509Opts.SerialNumber != nil {
-		crt.SerialNumber = *t.x509Opts.SerialNumber
+	if t.x509Opts.SerialNumber != "" {
+		i64, err := strconv.ParseInt(t.x509Opts.SerialNumber, 10, 2)
+		if err != nil {
+			return &x509.Certificate{}, err
+		}
+		crt.SerialNumber = big.NewInt(i64)
 	}
 
-	if t.x509Opts.Country != nil {
-		crt.Subject.Country = *t.x509Opts.Country
+	if t.x509Opts.Country != "" {
+		crt.Subject.Country = []string{t.x509Opts.Country}
 	}
 
-	if t.x509Opts.Organization != nil {
-		crt.Subject.Organization = *t.x509Opts.Organization
+	if t.x509Opts.Organization != "" {
+		crt.Subject.Organization = []string{t.x509Opts.Organization}
 	}
 
-	return crt
+	return crt, nil
 
 }
 
-func (t *TTLS)getX509Certificate() ([]byte, rsa.PrivateKey, error) {
+func (t *TTLS)getX509Certificate() ([]byte, *rsa.PrivateKey, error) {
 	privateKey, err := t.getPrivateKey()
 	if err != nil {
-		return []byte{}, rsa.PrivateKey{}, err
+		return []byte{}, &rsa.PrivateKey{}, err
 	}
-	template := t.getX509Template()
+	template, err := t.getX509Template()
+	if err != nil {
+		return []byte{}, &rsa.PrivateKey{}, err
+	}
 	x509crt, err := x509.CreateCertificate(rand.Reader, template, template, &privateKey.PublicKey, privateKey)
 	if err != nil {
 		return []byte{}, privateKey, err
